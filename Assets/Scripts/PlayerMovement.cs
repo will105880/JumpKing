baed73fc728bdad0ad02adf64b0c6e5fedbe787f
Will Blake <s105880@ltisdschools.net>
2024-05-08 11:24:36 -0500

    Push 2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D Player;
     public Transform playerbody;
+    public Rigidbody2D pointer;
     [SerializeField] public float force = 1;
     float time = 1;
     bool canjump = true;
@@ -23,22 +24,29 @@ public class PlayerMovement : MonoBehaviour
 
 
 
+
     private void Update()
-    {
+    { 
+
         jumptime += Time.deltaTime;
         Camera w = Camera.main;
         Vector3 x = (w.ScreenToWorldPoint(Input.mousePosition) - Player.transform.position).normalized;
+        float angle = Mathf.Atan2(x.y, x.x) * Mathf.Rad2Deg - 90;
+        pointer.rotation = angle;
+        pointer.position = Player.position + new Vector2(x.x, x.y).normalized * 1;
         x = new Vector2((x.x), (x.y)).normalized; 
         time += (Time.deltaTime * 200);
         if(check)
             playerbody.transform.localScale = new Vector2(.5f, (.5f / (time / 200))); 
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)) 
         {
+            if (Player.velocityY != 0)
+                canjump = false;
             float start = Time.fixedTime;
             time = 200f;
             check = true;
         }
-        if (Input.GetKeyUp(KeyCode.Space) == true)
+        if ((Input.GetKeyUp(KeyCode.Space) == true) || (Input.GetKeyUp(KeyCode.Mouse0) == true))
         {
             if (time >= 500f)
                 time = 500f;
